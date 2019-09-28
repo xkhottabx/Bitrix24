1d5bc4311b3e23de41cb1d1200392271bc87f1b0
xkhottabx <xkhottabx@gmail.com>
2019-09-28 11:39:43 -0400

    check

--- src/test/java/com/cybertek/pages/HomePage.java
+++ src/test/java/com/cybertek/pages/HomePage.java
@@ -2,7 +2,6 @@ package com.cybertek.pages;
 
         import com.cybertek.utilities.Driver;
         import org.openqa.selenium.By;
-        import org.openqa.selenium.WebDriver;
         import org.openqa.selenium.WebElement;
         import org.openqa.selenium.support.FindBy;
         import org.openqa.selenium.support.PageFactory;
